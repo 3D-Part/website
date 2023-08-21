25ff31df5787e17d88daa766fdaea0c3f88f71e5
Lemi Novic <milannovic61@gmail.com>
2023-08-21 14:05:05 +0200

    x

--- src/app/shop/category/[slug]/page.tsx
+++ src/app/shop/category/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import Container from "@/components/common/container/Container";
 import { notFound } from "next/navigation";
-import { isErrorObject } from "@/shared/interfaces/errorInterface";
 import { categoriesServices } from "../../../../../services/categoriesServices";
 import Description from "@/components/pages/shop/category/description/Description";
 import CategoryProducts from "@/components/pages/shop/category/category-products/CategoryProducts";
@@ -18,42 +17,47 @@ interface CategoryParams {
 export const generateMetadata = async ({
   params,
 }: CategoryParams): Promise<Metadata> => {
-  const data = await categoriesServices.getSingleCategoryWithSlug(params.slug);
+  try {
+    const data = await categoriesServices.getSingleCategoryWithSlug(
+      params.slug
+    );
 
-  return {
-    title: `${data.name}`,
-    description: `${data.description}`,
-    colorScheme: "dark",
-    openGraph: {
+    return {
+      title: `${data.name}`,
       description: `${data.description}`,
-      images: [
-        {
-          url: "/assets/img/logo_social.png",
-        },
-      ],
-    },
-  };
+      colorScheme: "dark",
+      openGraph: {
+        description: `${data.description}`,
+        images: [
+          {
+            url: "/assets/img/logo_social.png",
+          },
+        ],
+      },
+    };
+  } catch (error) {
+    return {
+      title: `404 Greška - Kategorija nije pronađena`,
+      description: `Kategorija nije pronađena`,
+    };
+  }
 };
 
 export default async function Category({ params }: CategoryParams) {
-  const categoryData = await categoriesServices.getSingleCategoryWithSlug(
-    params.slug
-  );
+  try {
+    const categoryData = await categoriesServices.getSingleCategoryWithSlug(
+      params.slug
+    );
 
-  if (isErrorObject(categoryData)) {
-    if (categoryData.key === "NOT_FOUND_ERROR") {
-      notFound();
-    } else {
-      // Handle other error cases if needed
-    }
+    return (
+      <div className="w-full">
+        <Container className="min-h-screen p-4 lg:px-8">
+          <Description description={categoryData.description} />
+          <CategoryProducts categoryId={categoryData.id} />
+        </Container>
+      </div>
+    );
+  } catch (error) {
+    notFound();
   }
-
-  return (
-    <div className="w-full">
-      <Container className="min-h-screen p-4 lg:px-8">
-        <Description description={categoryData.description} />
-        <CategoryProducts categoryId={categoryData.id} />
-      </Container>
-    </div>
-  );
 }
--- src/app/shop/product/[id]/page.tsx
+++ src/app/shop/product/[id]/page.tsx
@@ -18,63 +18,74 @@ interface ProductParams {
 export const generateMetadata = async ({
   params,
 }: ProductParams): Promise<Metadata> => {
-  const data = await productsServices.getSingleProduct(params.id);
+  try {
+    const data = await productsServices.getSingleProduct(params.id);
 
-  return {
-    title: `${data.name}`,
-    description: `${data.description}`,
-    colorScheme: "dark",
-    openGraph: {
-      images:
-        data.images.length > 0
-          ? [getMainImage(data.images)]
-          : ["/assets/img/logo_social.png"],
+    return {
+      title: `${data.name}`,
       description: `${data.description}`,
-    },
-  };
+      colorScheme: "dark",
+      openGraph: {
+        images:
+          data.images.length > 0
+            ? [getMainImage(data.images)]
+            : ["/assets/img/logo_social.png"],
+        description: `${data.description}`,
+      },
+    };
+  } catch (error) {
+    return {
+      title: `404 Greška - Proizvod nije pronađen`,
+      description: `Proizvod nije pronađen`,
+    };
+  }
 };
 
 export default async function Product({ params }: ProductParams) {
-  const data = await productsServices.getSingleProduct(params.id);
+  try {
+    const data = await productsServices.getSingleProduct(params.id);
 
-  if (isErrorObject(data)) {
-    if (data.key === "NOT_FOUND_ERROR") {
-      notFound();
-    } else {
-      // Handle other error cases if needed
+    if (isErrorObject(data)) {
+      if (data.key === "NOT_FOUND_ERROR") {
+        notFound();
+      } else {
+        // Handle other error cases if needed
+      }
     }
-  }
-  const similarProductsData = await productsServices.getAllProducts({
-    sku: data.sku,
-  });
+    const similarProductsData = await productsServices.getAllProducts({
+      sku: data.sku,
+    });
 
-  data.images.forEach((img, i) => {
-    if (img.isMain) {
-      const temp = data.images[0];
-      data.images[0] = data.images[i];
-      data.images[i] = temp;
-    }
-  });
+    data.images.forEach((img, i) => {
+      if (img.isMain) {
+        const temp = data.images[0];
+        data.images[0] = data.images[i];
+        data.images[i] = temp;
+      }
+    });
 
-  return (
-    <div className="w-full">
-      <Container className="min-h-screen px-4 lg:px-9">
-        <div className="w-full  border-solid border-b border-[rgba(242,242,242,0.3)] pt-6 pb-4 lg:flex lg:gap-14  lg:pt-8 lg:pb-14">
-          <div className="h-[380px] lg:h-[586px] lg:w-[586px] w-auto">
-            <Slider images={data.images} name={data.name} />
+    return (
+      <div className="w-full">
+        <Container className="min-h-screen px-4 lg:px-9">
+          <div className="w-full  border-solid border-b border-[rgba(242,242,242,0.3)] pt-6 pb-4 lg:flex lg:gap-14  lg:pt-8 lg:pb-14">
+            <div className="h-[380px] lg:h-[586px] lg:w-[586px] w-auto">
+              <Slider images={data.images} name={data.name} />
+            </div>
+            <MainData
+              productData={data}
+              similarProducts={similarProductsData.rows}
+            />
           </div>
-          <MainData
-            productData={data}
-            similarProducts={similarProductsData.rows}
-          />
-        </div>
 
-        <AdditionalData
-          detailedDescription={data.details}
-          productsAttributes={data.productAttributes}
-        />
-        <RelatedProducts categoryId={data.categoryId} productId={data.id} />
-      </Container>
-    </div>
-  );
+          <AdditionalData
+            detailedDescription={data.details}
+            productsAttributes={data.productAttributes}
+          />
+          <RelatedProducts categoryId={data.categoryId} productId={data.id} />
+        </Container>
+      </div>
+    );
+  } catch (error) {
+    notFound();
+  }
 }
