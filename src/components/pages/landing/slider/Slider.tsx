2ed660261abcc2e5733066301427a5106a782ee3
Lemi Novic <milannovic61@gmail.com>
2023-10-18 11:09:32 +0200

    wip

--- src/components/pages/landing/slider/Slider.tsx
+++ src/components/pages/landing/slider/Slider.tsx
@@ -9,9 +9,12 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import Container from "@/components/common/container/Container";
+import { useRouter } from "next/navigation";
 
 const Slider = () => {
   const modules = [Pagination, Navigation, Autoplay];
+
+  const router = useRouter();
 
   return (
     <Container className="w-full px-4 py-6 border border-white lg:px-9 lg:py-8 landing_slider">
@@ -47,13 +50,15 @@ const Slider = () => {
 
         <SwiperSlide>
           <SliderPage
-            bgUrl="/assets/img/slider/bg2.jpg"
-            productUrl="/assets/img/slider/product2.png"
+            bgUrl="/assets/img/slider/bg3.jpg"
+            productUrl="/assets/img/slider/product1.webp"
             title="Ender 3 S1 Pro"
             subtitle="Fulfill Big Ambitions Efficiently"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit elit sit amet faucibus vehicula. Nunc vel diam id nulla tincidunt mollis a at massa. In non molestie lacus, sed viverra erat. Praesent aliquet dui non vulputate fringilla. Fusce vulputate mauris non nisl aliquet."
+            description="Creality Ender 3 S1 Pro 3D printer 300℃ Visokotemperaturna mlaznica Sprite Full Metal Direct Drive Extruder CR Touch Auto Leveling Bed Silent Mainboard Filament Sensor Veličina ispisa 235x235x270"
             buttonText="Detaljnije"
-            buttonOnClick={() => {}}
+            buttonOnClick={() => {
+              router.push("/shop/product/8b2268b0-6ce6-11ee-a90e-137eed755a1c");
+            }}
             titleClassName="text-transparent from-[rgba(255,255,255,1)] to-[rgba(173,173,173,1)] bg-gradient-to-r bg-clip-text"
           />
         </SwiperSlide>
