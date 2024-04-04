c407b5b3a23f94425e6c187bbfacf22f21de4f47
Lemi17 <milannovic61@gmail.com>
2024-04-04 10:27:00 +0200

    favorite button

--- src/components/common/product/FavoriteButton.tsx
+++ src/components/common/product/FavoriteButton.tsx
@@ -6,11 +6,14 @@ import {
   changeSingleFavoriteProduct,
 } from "@/redux/slices/ui/uiSlice";
 import { favoritesService } from "@/shared/services/favoritesService";
+import { useSession } from "next-auth/react";
 import React, { FC } from "react";
 import { RiHeart3Line } from "react-icons/ri";
 import { RiHeart3Fill } from "react-icons/ri";
 
 const FavoriteButton: FC<{ productId: string }> = ({ productId }) => {
+  const { data: session } = useSession();
+
   const isFavorite = useAppSelector((state) =>
     isProductFavoriteSelector(state, productId)
   );
@@ -39,20 +42,24 @@ const FavoriteButton: FC<{ productId: string }> = ({ productId }) => {
   };
 
   return (
-    <div
-      onClick={(e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        clickHandler();
-      }}
-      className="absolute z-30 flex items-center justify-center w-8 h-8 rounded-full cursor-pointer top-1 right-1 bg-neutral-500 group "
-    >
-      {isFavorite ? (
-        <RiHeart3Fill className="w-5 h-5 transition-all duration-300 ease-in-out group-hover:scale-[1.2]" />
-      ) : (
-        <RiHeart3Line className="w-5 h-5 transition-all duration-300 ease-in-out group-hover:scale-[1.2]" />
+    <>
+      {session?.user && session && (
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            clickHandler();
+          }}
+          className="absolute z-30 flex items-center justify-center w-8 h-8 rounded-full cursor-pointer top-1 right-1 bg-neutral-500 group "
+        >
+          {isFavorite ? (
+            <RiHeart3Fill className="w-5 h-5 transition-all duration-300 ease-in-out group-hover:scale-[1.2]" />
+          ) : (
+            <RiHeart3Line className="w-5 h-5 transition-all duration-300 ease-in-out group-hover:scale-[1.2]" />
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
