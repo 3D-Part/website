af1e40b0365e25f3a4499366bd4829ddd7007701
Lemi17 <milannovic61@gmail.com>
2024-05-06 11:50:38 +0200

    x

--- src/components/pages/landing/products-recommended/ProductsRecommended.tsx
+++ src/components/pages/landing/products-recommended/ProductsRecommended.tsx
@@ -8,19 +8,21 @@ const ProductsRecommended = async () => {
 
   return (
     <Container className="w-full mb-[60px] lg:mb-24 lg:px-9">
-      <Products
-        products={data.rows}
-        className="mt-3 "
-        animationVariants={{
-          initial: { opacity: 0, scale: 0.7, x: -30 },
-          animate: { opacity: 1, scale: 1, x: 0 },
-        }}
-        reverseDirection={true}
-      >
-        <div className="flex flex-col w-full px-4 lg:px-0">
-          <Heading2> Izdvojili smo za Vas </Heading2>
-        </div>
-      </Products>
+      {data.rows.length !== 0 && (
+        <Products
+          products={data.rows}
+          className="mt-3 "
+          animationVariants={{
+            initial: { opacity: 0, scale: 0.7, x: -30 },
+            animate: { opacity: 1, scale: 1, x: 0 },
+          }}
+          reverseDirection={true}
+        >
+          <div className="flex flex-col w-full px-4 lg:px-0">
+            <Heading2> Izdvojili smo za Vas </Heading2>
+          </div>
+        </Products>
+      )}
     </Container>
   );
 };
